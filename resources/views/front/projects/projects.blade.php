1a5ef19373e0692135f2dcbd4936871c54b707d4
Viktoriia Iukhnina <viktoriia.iukhnina@thatzad.com>
2018-07-06 14:56:25 +0200

    [front] projects

--- resources/views/front/projects/projects.blade.php
+++ resources/views/front/projects/projects.blade.php
@@ -2,12 +2,44 @@
 
 @section('content')
 
-    <section class="service">
+    <section class="projects">
         <div class="container">
             <h2 class="title-page">Proyectos destacados</h2>
-            <p>Puedes filtrar los proyectos por sectores:</p>
-            <input type="checkbox" name="category" value="category" class="gray-radio"/>
-            <label for="category" class="gray-radio-label">TURISMO</label>
+            <p class="subtitle-projects">Puedes filtrar los proyectos por sectores:</p>
+            <div class="block-inputs">
+                <div class="check-category">
+                    <input type="checkbox" name="privacy" value="privacy" id="privacy" class="gray-radio"/>
+                    <label for="privacy" class="gray-radio-label blue">TURISMO</label>
+                </div>
+                <div class="check-category">
+                    <input type="checkbox" name="newsletter" value="newsletter" id="newsletter" class="gray-radio"/>
+                    <label for="newsletter" class="gray-radio-label blue">BANCA</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="inner-category">
+                        <img src="{{ asset('front/img/contact.png') }}" alt="">
+                        <p class="title-project">Turisimo de Canarias</p>
+                        <p class="title-category">TURISMO</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="inner-category">
+                        <p>Turisimo de Canarias</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="inner-category">
+                        <p>Turisimo de Canarias</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="inner-category">
+                        <p>Turisimo de Canarias</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </section>
