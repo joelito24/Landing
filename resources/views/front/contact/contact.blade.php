06278f0f497e832547c2fd17e66a11d9d57888a8
Viktoriia Iukhnina <viktoriia.iukhnina@thatzad.com>
2018-07-06 11:59:26 +0200

    [front] agency

--- resources/views/front/contact/contact.blade.php
+++ resources/views/front/contact/contact.blade.php
@@ -3,35 +3,107 @@
 <section class="contact">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <h2 class="title-contact">Contacto</h2>
-                <div class="text-contact">Nos encanta hablar de marketing online, campañas, e-commerce y proyectos digitales. No te cortes. Por favor, explícanos tu negocio y pensaremos juntos la mejor forma de hacer que sea un éxito.</div>
+            </div>
+            <div class="col-md-6">
+
+                <div class="text-contact">
+                    <p>Nos encanta hablar de marketing online, campañas, e-commerce y proyectos digitales.</p>
+                    <p>No te cortes. Por favor, explícanos tu negocio y pensaremos juntos la mejor forma de hacer que sea un éxito.</p>
+                </div>
+                <div class="contact-info">
+                    <div class="info-text">
+{{--                        <img src="{{ asset('front/img/contact/001-arroba.svg') }}" alt="">--}}
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                             viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                            <g>
+                                <g>
+                                    <path d="M255,0C114.06,0,0,114.05,0,255c0,139.895,113.025,257,255,257c140.644,0,257-115.914,257-257C512,112.87,394.743,0,255,0
+                                        z M255,482.2C130.935,482.2,29.8,380.168,29.8,255C29.8,130.935,130.935,29.8,255,29.8c125.168,0,227.2,101.135,227.2,225.2
+                                        C482.2,380.168,380.168,482.2,255,482.2z"/>
+                                </g>
+                            </g>
+                            <g>
+                                <g>
+                                    <path d="M255,90c-90.981,0-165,74.019-165,165c0,91.323,73.867,167,165,167c36.874,0,74.175-12.393,102.339-34.001
+                                c6.573-5.043,7.813-14.459,2.77-21.032c-5.043-6.573-14.46-7.813-21.032-2.77C316.048,381.866,285.403,392.2,255,392.2
+                                c-74.439,0-135-61.658-135-137.2c0-74.439,60.561-135.2,135-135.2c75.542,0,137,60.761,137,135.2v15c0,16.542-13.458,30-30,30
+                                s-30-13.458-30-30c0-7.43,0-67.164,0-75c0-8.284-6.716-15-15-15s-15,6.716-15,15v0.951C288.454,185.622,272.068,180,255,180
+                                c-41.355,0-75,33.645-75,75s33.645,75,75,75c22.423,0,43.059-9.622,57.735-25.812C323.583,319.772,341.615,330,362,330
+                                c33.084,0,60-26.916,60-60v-15C422,163.746,346.21,90,255,90z M255,300.2c-24.813,0-45-20.387-45-45.2s20.187-45.2,45-45.2
+                                c25.477,0,47,20.807,47,45.2S280.477,300.2,255,300.2z"/>
+                                </g>
+                            </g>
+                        </svg>
+                        <a class="link-contact" href="mailto:hola@thatzad.com">hola@thatzad.com</a>
+                    </div>
+                    <div class="info-text">
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                             viewBox="0 0 511.999 511.999" style="enable-background:new 0 0 511.999 511.999;" xml:space="preserve">
+                            <g>
+                                <g>
+                                    <path d="M498.827,377.633l-63.649-63.649c-17.548-17.547-46.102-17.547-63.649,0l-28.931,28.931
+                                        c-13.294,13.294-34.926,13.29-48.215,0.005l-125.4-125.507c-13.325-13.325-13.327-34.892,0-48.219
+                                        c4.66-4.66,18.041-18.041,28.931-28.931c17.471-17.47,17.715-45.935-0.017-63.665l-63.632-63.432
+                                        C116.717-4.381,88.164-4.381,70.663,13.12C57.567,26.102,53.343,30.29,47.471,36.111c-63.28,63.279-63.28,166.242-0.003,229.519
+                                        l198.692,198.796c63.428,63.429,166.088,63.434,229.521,0l23.146-23.145C516.375,423.733,516.375,395.181,498.827,377.633z
+                                         M91.833,34.382c5.849-5.849,15.365-5.85,21.233,0.016l63.632,63.432c5.863,5.863,5.863,15.352,0,21.216l-10.609,10.608
+                                        l-84.81-84.81L91.833,34.382z M267.38,443.213L68.687,244.415c-48.958-48.958-51.649-125.833-8.276-178.006l84.564,84.564
+                                        c-22.22,25.189-21.294,63.572,2.787,87.653l125.396,125.501c0.001,0.001,0.003,0.003,0.004,0.004
+                                        c24.055,24.056,62.436,25.042,87.656,2.792l84.566,84.566C393.377,494.787,316.675,492.508,267.38,443.213z M477.612,420.065
+                                        l-10.609,10.609l-84.865-84.866l10.607-10.608c5.85-5.849,15.367-5.85,21.217,0l63.649,63.649
+                                        C483.461,404.699,483.461,414.217,477.612,420.065z"/>
+                                </g>
+                            </g>
+                        </svg>
+                        <a class="link-contact" href="">+34 936350620</a>
+                    </div>
+                    <div class="info-text">
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                             viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+                            <g>
+                                <g>
+                                    <path d="M256,0C156.748,0,76,80.748,76,180c0,33.534,9.289,66.26,26.869,94.652l142.885,230.257
+                                        c2.737,4.411,7.559,7.091,12.745,7.091c0.04,0,0.079,0,0.119,0c5.231-0.041,10.063-2.804,12.75-7.292L410.611,272.22
+                                        C427.221,244.428,436,212.539,436,180C436,80.748,355.252,0,256,0z M384.866,256.818L258.272,468.186l-129.905-209.34
+                                        C113.734,235.214,105.8,207.95,105.8,180c0-82.71,67.49-150.2,150.2-150.2S406.1,97.29,406.1,180
+                                        C406.1,207.121,398.689,233.688,384.866,256.818z"/>
+                                </g>
+                            </g>
+                                                        <g>
+                                                            <g>
+                                                                <path d="M256,90c-49.626,0-90,40.374-90,90c0,49.309,39.717,90,90,90c50.903,0,90-41.233,90-90C346,130.374,305.626,90,256,90z
+                                         M256,240.2c-33.257,0-60.2-27.033-60.2-60.2c0-33.084,27.116-60.2,60.2-60.2s60.1,27.116,60.1,60.2
+                                        C316.1,212.683,289.784,240.2,256,240.2z"/>
+                                                            </g>
+                                                        </g>
+                        </svg>
+                        <p>C/ Marqués de Mulhacen 11, bajos 2. 08034 Barcelona</p>
+                        <a class="link-map" target="_blank" href="https://www.google.es/maps/place/Thatzad!+That's+Advertising./@41.3910271,2.1175009,17z/data=!3m1!4b1!4m5!3m4!1s0x12a482af16c1db65:0xd098ab6be5c69c5c!8m2!3d41.3910231!4d2.1196949?hl=es">Ver mapa</a>
+                    </div>
+                </div>
             </div>
             <div class="col-md-6">
                 <div class="form-block">
                     <form method="POST" action="{{ action('ContactController@send') }}" id="contactform">
                         <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
                         <div class="form-group">
-                            <label>Nombre</label>
-                            <input class="form-control required" type="text" name="name" id="name">
+                            <input placeholder="Nombre" class="form-control required" type="text" name="name" id="name">
                         </div>
                         <div class="form-group">
-                            <label>Empresa</label>
-                            <input class="form-control" type="text" name="company" id="company">
+                            <input placeholder="Empresa" class="form-control" type="text" name="company" id="company">
                         </div>
                         <div class="form-group">
-                            <label>Email (obligatorio)</label>
-                            <input class="form-control required" type="text" name="email" id="email">
+                            <input placeholder="Email (obligatorio)" class="form-control required" type="text" name="email" id="email">
                         </div>
                         <div class="form-group">
-                            <label>Teléfono</label>
-                            <input class="form-control" type="text" name="telephone" id="telephone">
+                            <input placeholder="Teléfono" class="form-control" type="text" name="telephone" id="telephone">
                         </div>
                         <div class="form-group">
-                            <label>Web</label>
-                            <input class="form-control" type="text" name="web" id="web">
+                            <input placeholder="Web" class="form-control" type="text" name="web" id="web">
                         </div>
-                        <div class="form-group checks">
+                        <div class="form-group checks margin0">
                             <label>Áreas de consulta:</label>
                             {{--<label for="consult1"><input class="form-check-input" type="checkbox" name="consult1" id="consult1" value="1">Consultoría general</label>--}}
                             {{--<label for="consult2"><input class="form-check-input" type="checkbox" name="consult2" id="consult2" value="2">SEO</label>--}}
@@ -59,14 +131,18 @@
                         </div>
                         <div class="form-check">
                             <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="newsletter" value="1">
-                                <span class="acept-text">Acepto suscribirme a la newsletter de esta web.</span>
+                                <input type="checkbox" name="newsletter" value="newsletter" id="newsletter" class="gray-radio"/>
+                                <label for="newsletter" class="gray-radio-label">Acepto suscribirme a la newsletter de esta web.</label>
+                                {{--<input class="form-check-input" type="checkbox" name="newsletter" value="1">--}}
+                                {{--<span class="acept-text">Acepto suscribirme a la newsletter de esta web.</span>--}}
                             </label>
                         </div>
                         <div class="form-check">
                             <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="privacy" id="privacy" value="1">
-                                <span class="acept-text">Acepto la política de privacidad aplicada en esta web.</span>
+                                <input type="checkbox" name="privacy" value="privacy" id="privacy" class="gray-radio"/>
+                                <label for="privacy" class="gray-radio-label">Acepto la política de privacidad aplicada en esta web.</label>
+                                {{--<input class="form-check-input" type="checkbox" name="privacy" id="privacy" value="1">--}}
+                                {{--<span class="acept-text">Acepto la política de privacidad aplicada en esta web.</span>--}}
                             </label>
                         </div>
                             <div class="send" id="response"><input class="submit btn-yellow-full" type="button" id="send" value="Enviar"></div>
@@ -93,6 +169,10 @@
             }
         });*/
         $(document).ready(function(){
+
+            $('#header').addClass('header_transparent');
+            $('#header').css('position', 'absolute');
+
             $("#send").click(function(e){
                 e.preventDefault();
                 var error = 0;
--- resources/views/front/static/agency.blade.php
+++ resources/views/front/static/agency.blade.php
@@ -2,8 +2,40 @@
 
 @section('content')
 
-    <section class="service">
-        Thatzad Agencia
+    <section class="agency">
+        <div class="top-agency">
+            <div class="container">
+                <p class="name-page">Agencia</p>
+                <h1 class="title">Una agencia boutique de grandes proyectos</h1>
+                <h2 class="subtitle">Marketing por vocación. Digitales por naturaleza.</h2>
+            </div>
+        </div>
+        <div class="content-agency">
+            <div class="container">
+                <div class="description">
+                    <div class="col-md-6">
+                        <p class="text-desc">El éxito de un negocio digital es una mezcla de Conocimiento, Creatividad y Esfuerzo. Pero el Conocimiento se multiplica cuando se trabaja en equipo, la Creatividad se amplifica cuando todos aportan ideas y el Esfuerzo se disfruta cuando te mueve la pasión y las ganas de ir un paso más allá y hacer las cosas mucho mejor.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="phrase">Nos sentimos tremendamente orgullosos de idear, lanzar y hacer crecer negocios digitales.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-agency">
+            <div class="inner-agency">
+                <div class="text-col">
+                    <p>Pero para hacerlo realmente bien, no podemos tener cien clientes, no sabemos hacer webs, e-shops o campañas online como si fueran una cadena de montaje. No nos obsesiona optimizar los tiempos de producción, maximizar la rentabilidad o conseguir volumen para bajar costes. Sólo nos concentramos en que los proyectos superen las expectativas de nuestros Clientes.</p>
+                    <p>Esa es nuestra idea de Agencia Boutique, grandes proyectos por su calidad, no por su tamaño. Proyectos motivantes, que ilusionen al equipo, ya que son ellos los que deberán ir un paso más allá con su creatividad, ejecutando las campañas, analizando los resultados y dándole una vuelta y otra más para dar con las claves que consigan los objetivos marcados.</p>
+                    <p>Esta ha sido nuestra forma de entender lo que queríamos que fuera nuestra agencia de marketing online durante los últimos 12 años, nosotros estamos orgullosos de haberlo conseguido y nos gusta pensar que nuestros Clientes opinan lo mismo.</p>
+                </div>
+                <div class="img-col">
+                    <img src="{{ asset('front/img/agency/agency.png') }}" alt="">
+                </div>
+            </div>
+
+        </div>
+
     </section>
 
 @stop
@@ -11,4 +43,13 @@
 @section('titles')
     <title>Thatzad</title>
     <meta name="description" content="">
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+            $('#header').addClass('header_transparent');
+            $('#header').css('position', 'absolute');
+        });
+    </script>
 @endsection
