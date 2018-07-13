76e1de1610c9237b4948533cc6b3d522cd1eca2e
Cristian Fernandez <cristiann_96@hotmail.es>
2018-07-13 12:06:10 +0200

    Maquetación y programacion back servicios

--- resources/views/front/services/service.blade.php
+++ resources/views/front/services/service.blade.php
@@ -16,40 +16,29 @@
     		<div class="container">
 	    		<div class="row">
 
-	    			<div class="col-sm-12 col-md-6 top-page @if($service->about == "<div><br></div><div><br></div>") text-page @endif">
-						@if($service->about == "<div><br></div><div><br></div>")
+	    			<div class="col-sm-12 col-md-5 @if(empty(strip_tags($service->about))) text-page @else top-page @endif">
+						{!! $service->about !!}
+						<div class="text-page" style="@if(!empty(strip_tags($service->about))) margin-top:50px; color: unset; @endif">
 							<div class="description1">{!! $service->description1 !!}</div>
+							<div class="quote-mbl">
+								<div class="quote">"{!! $service->quote !!}"</div>
+							</div>
 							<div class="description2">{!! $service->description2 !!}</div>
-						@endif
-	        			{!! $service->about !!}
+						</div>
 	    			</div>
-					<div class="col-sm-12 col-md-6">
-						@if($service->about != "<div><br></div><div><br></div>")
-							<div class="quote">"{!! $service->quote !!}"</div>
-						@endif
-						@if($service->about == "<div><br></div><div><br></div>")
-								<div class="row">
-									<div class="quote">"{!! $service->quote !!}"</div>
-								</div>
+					<div class="col-md-2"></div>
+					<div class="col-sm-12 col-md-5">
+							<div class="row quote-desk">
+								<div class="quote">"{!! $service->quote !!}"</div>
+							</div>
 							<div class="row">
 								<img class="img-responsive" src="{{ $service->image1 }}">
 							</div>
-						@endif
 
 						<!--<img src="{{ $service->image1 }}">-->
 					</div>
 				</div>
-				@if($service->about != "<div><br></div><div><br></div>")
-				<div class="row">
-					<div class="col-sm-12 col-md-6 text-page">
-						<div class="description1">{!! $service->description1 !!}</div>
-						<div class="description2">{!! $service->description2 !!}</div>
-					</div>
-					<div class="col-sm-12 col-md-6" style="text-align: center">
-						<img class="img-responsive" src="{{ $service->image1 }}">
-					</div>
-				</div>
-				@endif
+
 
 
 
@@ -57,7 +46,7 @@
 			</div>
 		</div>
 
-    	<div class="conclusion-service">
+    	<div class="conclusion-service" style="background: url({{ $service->image2 }}) no-repeat center center">
     		<div class="container">
     			<div class="row">
     				<div class="col-sm-12 col-md-6">
@@ -82,27 +71,27 @@
 								<form method="POST" action="" id="contactform">
 									<input name="_token" type="hidden" value="{{ csrf_token() }}"/>
 									<div class="form-group">
-										<label>Nombre</label>
-										<input class="form-control required" type="text" name="name" id="name">
+										<!--<label>Nombre</label>-->
+										<input class="form-control required" type="text" name="name" id="name" placeholder="Nombre">
 									</div>
 									<div class="form-group">
-										<label>Email (obligatorio)</label>
-										<input class="form-control required" type="text" name="email" id="email">
+										<!--<label>Email (obligatorio)</label>-->
+										<input class="form-control required" type="text" name="email" id="email" placeholder="Email">
 									</div>
 									<div class="form-group">
-										<label>Comentario</label>
+										<!--<label>Comentario</label>-->
 										<textarea class="form-control" placeholder="Cuéntanos más detalles para entender mejor tu negocio" id="consulta" name="consulta"></textarea>
 									</div>
 									<div class="form-check">
 										<label class="form-check-label">
-											<input class="form-check-input" type="checkbox" name="newsletter" value="1">
-											<span class="acept-text">Acepto suscribirme a la newsletter de esta web.</span>
+											<input type="checkbox" name="newsletter" value="1" id="newsletter" class="gray-radio"/>
+											<label for="newsletter" class="white-radio-label">Acepto suscribirme a la newsletter de esta web.</label>
 										</label>
 									</div>
 									<div class="form-check">
 										<label class="form-check-label">
-											<input class="form-check-input" type="checkbox" name="privacy" id="privacy" value="1">
-											<span class="acept-text">Acepto la política de privacidad aplicada en esta web.</span>
+											<input class="gray-radio" type="checkbox" name="privacy" id="privacy" value="1">
+											<label for="privacy" class="white-radio-label">Acepto la política de privacidad aplicada en esta web.</label>
 										</label>
 									</div>
 									<div class="send" id="response"><input class="submit btn-yellow-full" type="button" id="send" value="Enviar"></div>
