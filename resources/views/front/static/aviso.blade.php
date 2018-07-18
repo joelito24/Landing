757e9222f4224405b367e6d79d5541d5fdb3cd03
Viktoriia Iukhnina <viktoriia.iukhnina@thatzad.com>
2018-07-18 11:36:05 +0200

    [front] generales

--- resources/views/front/static/aviso.blade.php
+++ resources/views/front/static/aviso.blade.php
@@ -2,9 +2,41 @@
 
 @section('content')
 
-    <section class="content aviso">
-        <div class="col-md-12">
-            <h2>Aviso legal</h2>
+    <section class="generals">
+        <div class="container">
+            <div class="row">
+                <h2 class="title-page">Aviso legal</h2>
+                <p class="subtitle">Objeto de la web</p>
+                <p>La web <a href="http://www.thatzad.com/">www.thatzad.com</a> se ha realizado con objeto comercial, dispone información que se ofrece de forma gratuita y accesible para todo el mundo sin necesidad de abonar ningún importe en el momento de acceso pero puede tener secciones para las que estas condiciones cambien, ya sea por ofrecer contenidos de pago o por que el acceso esté restringido a usuarios determinados.</p>
+                <p>Ser usuario de la web de <a href="http://www.thatzad.com/">www.thatzad.com</a> implica que reconoce haber leído y aceptado las presentes condiciones y lo que las extienda la normativa legal aplicable en esta materia. Si Vd. no está de acuerdo con estas condiciones le rogamos que no continúe usando esta web.</p>
+                <p class="subtitle">Propiedad intelectual e industrial</p>
+                <p>El sitio web <a href="http://www.thatzad.com/">www.thatzad.com</a> es un dominio registrado por That’s Advertising, S.L. En cumplimiento del artículo 10 de la ley 34/2002, de 11 de julio, de Servicios de la Sociedad de la Información y de Comercio Electrónico (LSSI), se exponen los datos identificativos y conforme al principio de transparencia de nuestra empresa:</p>
+                <ul class="info">
+                    <li>Denominación Social: <b>That’s Advertising, S.L., en adelante, Thatzad</b></li>
+                    <li>Domicilio social: <b>C/ Saragossa 1 Local 16, Castelldefels 08860 - Barcelona</b></li>
+                    <li>C.I.F. número: <b>B.65645145</b></li>
+                    <li>Inscrita en el registro mercantil: <b>Barcelona, Tomo: 42788, Folio: 44, Hoja: 412853, Inscripción: 1</b></li>
+                    <li>Teléfono de contacto: <b>+34 936 350 620</b></li>
+                </ul>
+                <p>Este dominio y la marca Thatzad, Thatzad.com o That’s Advertising no pueden ser utilizados, salvo previa autorización expresa y por escrito de <b>Thatzad</b>.</p>
+                <p>Thatzad ostenta todos los derechos sobre el contenido, diseño de look&feel, diseño de elementos gráficos (botones, iconos,…), textos, arquitectura y código fuente del sitio web. En especial, con carácter enunciativo pero no limitativo, sobre las fotografías, infografías, imágenes, esquemas, logos propios, marcas, nombres comerciales y datos que se incluyen en el sitio web.</p>
+                <p>Asimismo queda totalmente prohibida la copia, reproducción, adaptación, modificación, distribución, comercialización, comunicación pública y/o cualquier otra acción que comporte una infracción de la normativa vigente española y/o internacionales en materia de propiedad intelectual y/o industrial, así como el uso de los contenidos de la web si no es con la previa autorización expresa y por escrito de <b>Thatzad</b>.</p>
+                <p class="subtitle">Responsabilidad</p>
+                <p><b>Thatzad</b> no se hace responsable de los daños y perjuicios que se produzcan por fallos o malas configuraciones del software instalado en el ordenador del Usuario, no garantiza la inexistencia de interrupciones o errores en el acceso al sitio web, ni asumirá responsabilidad alguna por problemas técnicos o fallos que se produzcan durante la conexión a Internet.</p>
+                <p>Esta web está preparada por defecto para ser visualizado en navegadores de nueva generación por lo que pueden existir fallos de funcionamiento con navegadores antiguos o con configuraciones personalizadas de los mismos.</p>
+                <p>Tampoco garantiza la ausencia de virus ni de otros elementos que puedan producir alteraciones en su sistema informático (software y hardware) o en los documentos electrónicos y ficheros almacenados en su sistema informático, por lo que excluye cualquier responsabilidad por los daños y perjuicios que puedan producirse. El usuario deberá tomar las medidas de seguridad adecuadas para proteger la información alojada en sus equipos, para evitar pérdidas o daños causados por el acceso o las descargas desde <a href="http://www.thatzad.com/">www.thatzad.com</a></p>
+                <p>Thatzad no se hace tampoco responsable de los daños o perjuicios que puedan derivar del uso de la información y artículos contenidos en esta web, ni en las páginas a las que se enlacen desde <a href="http://www.thatzad.com/">www.thatzad.com</a></p>
+                <p>Para toda cuestión litigiosa o que incumba a la web de <b>Thatzad</b>, será de aplicación la legislación española, siendo competentes para la resolución de todos los conflictos derivados o relacionados con el uso de esta página web, los Juzgados y Tribunales de Barcelona (España).</p>
+                <p class="subtitle">Licencia de enlaces</p>
+                <p>Todo enlace externo a www.thatzad.com deberá cumplir las siguientes condiciones:</p>
+                <p>El usuario no podrá reproducir, ni imitar, total o parcialmente, el contenido de la web, los logotipos y marcas, ni la apariencia gráfica del mismo. Tampoco podrá realizar frames o marcos de las páginas de Thatzad.</p>
+                <p>No se realizarán manifestaciones o indicaciones falsas, inexactas, incorrectas o denigratorias sobre el <b>Thatzad</b>, su página web o cualquiera de sus empleados o socios. Tampoco se hará mención, directa o indirectamente que se colabora, se trabaja o se pertenece a <b>Thatzad</b>.</p>
+                <p>La página web en la que se establezca el enlace no contendrá informaciones o contenidos ilícitos, contrarios a la moral a las buenas costumbres generalmente aceptadas y al orden público. Tampoco podrá ser una granja de enlaces o tener enlaces o contenidos que puedan ser penalizados por parte de cualquier buscador.</p>
+                <p>En el caso en que <b>Thatzad</b> de permiso por escrito para la publicación de contenido, textos o imágenes de la web de <b>Thatzad</b> este contenido debe identificar claramente al autor y tener, al menos, un enlace visible a los buscadores (“do follow”) que dirija a <a href="http://www.thatzad.com/">www.thatzad.com</a> o a alguna de sus páginas internas.</p>
+                <p>Cualquier otra pretensión de enlace distinta de la estipulada en la presente cláusula requerirá la previa aceptación por escrito de <b>Thatzad</b>.</p>
+                <p class="subtitle">Modificaciones</p>
+                <p><b>Thatzad</b> podrá variar las presentes condiciones en cualquier momento, aunque no es intención que sea frecuente. Si por cambio legislativo o resolución judicial deja de ser aplicable una parte de las presentes condiciones, dichas cláusulas serán anuladas, pero las demás serán perfectamente válidas y permanecerán en vigor. El hecho de continuar usando esta web implica que el usuario acepta estas condiciones y se compromete a aceptar las modificaciones realizadas en las mismas.</p>
+            </div>
 
         </div>
     </section>
