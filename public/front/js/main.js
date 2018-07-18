77db0cd0b0c0395192308c40e2d896b604f720bf
Cristian Fernandez <cristiann_96@hotmail.es>
2018-07-18 12:36:04 +0200

    Cambios en css para servicios y menu

--- public/front/js/main.js
+++ public/front/js/main.js
@@ -7,17 +7,21 @@ $(document).ready(function(){
         $(".mobile-dropdown").slideToggle();
         $("header .mbl-servicios").toggleClass("default-color");
         $("header .mbl-servicios").toggleClass("main-blue");
-        $("header .mbl-servicios img").toggleClass("rotated");
+        $("header .mbl-servicios span").toggleClass("down");
+        $("header .mbl-servicios span").toggleClass("rotated");
+
     });
     $("header .mbl-especializaciones").click(function(){
         $(".esp-mobile-dropdown").slideToggle();
         $("header .mbl-especializaciones").toggleClass("default-color");
         $("header .mbl-especializaciones").toggleClass("main-blue");
-        $("header .mbl-especializaciones img").toggleClass("rotated");
+        $("header .mbl-especializaciones span").toggleClass("down");
+        $("header .mbl-especializaciones span").toggleClass("rotated");
     });
     $("#burger, #mobile-close").click(function(){
         $("#header").toggleClass('pos-abs');
-        $("#burger, #mobile-close").toggle();
+        //$("#burger, #mobile-close").toggle();
         $("#mobile-menu-content").toggle();
+        $(this).find('#nav-icon1').toggleClass('open');
     });
 });
