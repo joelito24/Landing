877fdad6d82d4105bacc615636f34ea15e57aeb3
Viktoriia Iukhnina <viktoriia.iukhnina@thatzad.com>
2018-08-20 10:19:27 +0200

    [route] change name

--- resources/lang/es/routes.php
+++ resources/lang/es/routes.php
@@ -11,7 +11,7 @@ return [
     'agency'                => 'agencia',
     'specialization1'       => 'proyectos-de-e-commerce',
     'specialization2'       => 'publicidad-online-orientada-a-resultados',
-    'specialization3'       => 'e-marketing-y-publicidad-para-mareas',
+    'specialization3'       => 'e-marketing-y-publicidad-para-marcas',
     'specialization4'       => 'transformacion-digital-para-empresas',
     'aviso'                 => 'aviso-legal',
     'politica'              => 'politica-de-cookies',
